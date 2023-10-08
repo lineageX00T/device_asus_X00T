71e5ec018209b3d18acdaebd7362e311e5e8c358
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-10-08 19:07:37 +0530

    X00TD: Build android.frameworks.sensorservice
     * Needed to get cameras working on Android 14.
    
    Signed-off-by: karthik1896 <karthik1896@outlook.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- device.mk
+++ device.mk
@@ -78,6 +78,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.common@1.0 \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
