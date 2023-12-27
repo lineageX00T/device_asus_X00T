3edc4e48af79e7d10dc8bf8a2c881d68cc618286
fakedotkit <ewprjkt@proton.me>
2023-12-27 05:56:54 +0000

    X00TD: Add vendorsetup
    Signed-off-by: fakedotkit <ewprjkt@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,7 @@ BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_IMAGE_NAME  := Image.gz-dtb
 
 TARGET_KERNEL_SOURCE := kernel/asus/sdm660
-TARGET_KERNEL_CONFIG := vendor/X00TD_defconfig vendor/debugfs.config
+TARGET_KERNEL_CONFIG := asus/X00TD_defconfig vendor/debugfs.config
 TARGET_KERNEL_VERSION := 4.19
 
 # LMKD
---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf vendor/asus/X00TD
+git clone https://github.com/ProjectBlaze-Devices/vendor_asus_X00TD -b udc vendor/asus/X00TD
+
+rm -rf kernel/asus/sdm660
+git clone --recursive https://github.com/RyuujiX/android_kernel_asus_sdm660-4.19 kernel/asus/sdm660
+
