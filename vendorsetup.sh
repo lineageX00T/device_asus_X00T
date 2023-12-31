1642a681aa78e7cb4cd1592ca0beb462124c678a
fakedotkit <ewprjkt@proton.me>
2023-12-31 09:03:48 +0000

    X00TD: Change kernel tree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,4 @@ rm -rf vendor/asus/X00TD
 git clone https://github.com/ProjectBlaze-Devices/vendor_asus_X00TD -b udc vendor/asus/X00TD
 
 #rm -rf kernel/asus/sdm660
-git clone --recursive https://github.com/RyuujiX/android_kernel_asus_sdm660-4.19 kernel/asus/sdm660
-
+git clone --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660-4.19 -b main kernel/asus/sdm660
