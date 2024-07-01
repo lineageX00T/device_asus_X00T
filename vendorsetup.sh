2ed6c376a98561ab5ea30b2962a8aaf2d7eaa51d
Meisie Widuri <ewprjkt@proton.me>
2024-07-02 05:15:33 +0700

    X00T: recursive

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,7 +19,7 @@ rm -rf hardware/nad/interfaces
 rm -rf vendor/lineage-priv
 
 git clone --depth=1 https://github.com/lineageX00T/android_vendor_asus -b lineage-17.1 vendor/asus
-git clone --depth=1 https://github.com/Tiktodz/android_kernel_asus_sdm636 -b staging kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b staging kernel/asus/sdm660
 git clone --depth=1 https://github.com/lineageX00T/android_build -b lineage-17.1 build/make
 git clone --depth=1 https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio -b lineage-17.1 hardware/qcom-caf/msm8998/audio
 git clone --depth=1 https://github.com/lineageX00T/android_frameworks_av -b lineage-17.1 frameworks/av
