ca75b3905e20046af24cc6b3a664ea7e042560d6
texascake soft <budalblimbing@gmail.com>
2024-11-27 18:03:49 +0000

    X00T: Update for revived
    Change-Id: I23a32917af08f9a2a61f0c83e1e5ae965e3fe759

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,39 +1,19 @@
-rm -rf device/asus/X00TD
-rm -rf device/asus/sdm660-common
-rm -rf vendor/asus
-rm -rf kernel/asus/sdm660
-
-rm -rf build/make
+# Audio
 rm -rf hardware/qcom-caf/msm8998/audio
-rm -rf frameworks/av
-rm -rf frameworks/base
-rm -rf lineage-sdk
-rm -rf device/lineage/sepolicy
-rm -rf packages/apps/Settings
-rm -rf packages/apps/Updater
-rm -rf vendor/lineage
-rm -rf external/motorola/faceunlock
-rm -rf external/asus/stitchimage
-rm -rf system/core
-rm -rf hardware/nad/interfaces
-rm -rf vendor/lineage-priv
+git clone --depth=1 https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio hardware/qcom-caf/msm8998/audio
 
-git clone --depth=1 https://github.com/lineageX00T/android_vendor_asus -b lineage-17.1 vendor/asus
-git clone --depth=1 --recursive https://github.com/texascake/kernel_asus_sdm660 kernel/asus/sdm660
+# Kernel
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/texascake/kernel_asus_sdm660 -b tom/hmp kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/docs
+rm -rf kernel/asus/sdm660/KernelSU/scripts
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 rm -rf kernel/asus/sdm660/KernelSU/website
-git clone --depth=1 https://github.com/lineageX00T/android_build -b lineage-17.1 build/make
-git clone --depth=1 https://github.com/lineageX00T/hardware_qcom-caf_msm8998_audio -b lineage-17.1 hardware/qcom-caf/msm8998/audio
-git clone --depth=1 https://github.com/lineageX00T/android_frameworks_av -b lineage-17.1 frameworks/av
-git clone --depth=1 https://github.com/lineageX00T/android_frameworks_base -b lineage-17.1 frameworks/base
-git clone --depth=1 https://github.com/lineageX00T/android_lineage-sdk -b lineage-17.1 lineage-sdk
-git clone --depth=1 https://github.com/lineageX00T/android_device_lineage_sepolicy -b lineage-17.1 device/lineage/sepolicy
-git clone --depth=1 https://github.com/lineageX00T/android_packages_apps_Settings -b lineage-17.1 packages/apps/Settings
-git clone --depth=1 https://github.com/lineageX00T/android_packages_apps_Updater -b lineage-17.1 packages/apps/Updater
-git clone --depth=1 https://github.com/lineageX00T/android_vendor_lineage -b lineage-17.1 vendor/lineage
-git clone --depth=1 https://gitlab.com/rr-os/external_utils -b Q external/motorola/faceunlock
-git clone --depth=1 https://github.com/pixelexperience/external_asus_stitchimage -b ten-plus external/asus/stitchimage
-git clone --depth=1 https://github.com/lineageX00T/los_system_core -b lineage-17.1 system/core
-git clone --depth=1 https://github.com/Tiktodz/vendor -b 17.1 keymoment && cp -R keymoment/* vendor/ && rm -rf keymoment
 
-export TZ=Asia/Jakarta
+# Vendor
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/Tiktodz/vendor_asus -b lineage-17.1 vendor/asus
+
+# Vendor keys
+rm -rf vendor/lineage-priv
+git clone https://github.com/texascake/vendor -b 10 keymoment && cp -R keymoment/* vendor/ && rm -rf keymoment
